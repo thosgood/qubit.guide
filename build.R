1394e9200d5a788767ed07c09d7dbe118cab5eb9
Tim Hosgood <timhosgood@gmail.com>
2021-01-15 01:38:10 +0000

    pdf build plus lots of small latex fixes

--- build.R
+++ build.R
@@ -4,9 +4,8 @@ bookdown::render_book("index.Rmd",
                       "bookdown::gitbook",
                       output_dir = "book",
                       config_file = "_bookdown.yml")
-#bookdown::render_book("index.Rmd",
-#                      "bookdown::pdf_book",
-#                      output_dir = "book",
-#                      config_file = "_bookdown.yml")
-
+bookdown::render_book("index.Rmd",
+                      "bookdown::pdf_book",
+                      output_dir = "book",
+                      config_file = "_bookdown.yml")
 print('Done!')
